df80e4dd6002557e446bef9af2a625a4e876d1d5
Catherine Kawara <ndutakawara2@gmail.com>
2022-04-28 15:24:02 +0300

    complete assignment

--- src/components/About.js
+++ src/components/About.js
@@ -2,7 +2,11 @@ import React from "react";
 import { image } from "../data/data";
 
 function About() {
-  return <div>About</div>;
+  return <div id="about">
+    <h2>About Me</h2>
+    <p>So, What about me?.... Weeellll....</p>
+    <img src ={image} alt="I made this" ></img>
+  </div>;
 }
 
 export default About;
